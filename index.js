f0a8959d71d994172d02d49f163679d595da0ccc
bluewyvern4 <bluewyvern4@gmail.com>
2017-07-09 21:28:25 -0700

    Bind to port; attempt.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
 var async = require("async");
 var Discord = require('discord.io');
 
